783c7c5e4f91ff295e9f896222178da7027bfabe
Chris Foulstone <chris@impelling.co.uk>
2023-12-19 18:13:17 +0000

    Get partner info added

---
+++ CoveBackupApi/Public/Get-CovePartnerInfo.ps1
@@ -0,0 +1,37 @@
+function Get-CovePartnerInfo {
+    <#
+    .SYNOPSIS
+        Gets the partner information for the Cove API
+    .DESCRIPTION
+        Gets the partner information for the Cove API, using the credentials stored in the script
+    .EXAMPLE
+        Get-CovePartnerInfo -Verbose
+        Gets the partner information for the Cove API
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+        $params = @{
+            CoveMethod = 'GetPartnerInfo'
+            Params = @{
+                name = $Script:CoveApiCredentials.Partner
+            }
+        }
+        $Data = Invoke-CoveApiRequest @params
+        if ($Data) {
+            return $Data
+        }
+        Write-Error "Failed to get partner info"
+    }
+
+    end {
+
+    }
+}
