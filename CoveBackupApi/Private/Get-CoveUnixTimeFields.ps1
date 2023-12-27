c9bf61b41727497a5b650762fc4b2f0a314e35b8
Chris Foulstone <chris@impelling.co.uk>
2023-12-27 19:16:55 +0000

    Add unix time conversion and field lookup

---
+++ CoveBackupApi/Private/Get-CoveUnixTimeFields.ps1
@@ -0,0 +1,42 @@
+function Get-CoveUnixTimeFields {
+    <#
+    .SYNOPSIS
+        Gets the Unix fields for the Cove API
+    .DESCRIPTION
+        Gets the Unix fields for the Cove API
+    .EXAMPLE
+        Get-CoveUnixFields -Verbose
+        Returns the Unix fields for the Cove API
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+        $UnixTimeFields = @(
+            'CreationTime',
+            'TrialExpirationTime',
+            'TrialRegistrationTime',
+            'Timestamp',
+            'I6',
+            'Expiration date',
+            'I5',
+            'Creation date',
+            'I4',
+            'F09', # Last successful session timestamp
+            'F15', # Last session timestamp
+            'F18' # Last completed session timestamp
+        )
+        return $UnixTimeFields
+
+    }
+
+    end {
+
+    }
+}
